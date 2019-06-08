16fd9b218e9dd25185c120b597079cef21d244dc
Michal Zakrzewski <michal.zakrzewski@westwing.pl>
2019-06-08 22:21:26 +0200

    Progress...

--- Decoder.py
+++ Decoder.py
@@ -1,19 +1,22 @@
-import PIL
 import numpy as np
 import pandas as pd
 from IPython.core.display import display
 import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+import PIL
 
-# ../input/train_ship_segmentations_v2.csv for Windows, ./input/train_ship_segmentations_v2.csv for MacOS
-df = pd.read_csv("./input/train_ship_segmentations_v2.csv", index_col=0).dropna()
-display(df.head())
-df['EncodedPixels']['000155de5.jpg']
+
+# ../input/train_ship_segmentations_v2.csv for Windows and Kaggle, ./input/train_ship_segmentations_v2.csv for MacOS
+df_tmp = pd.read_csv("./input/train_ship_segmentations_v2.csv").dropna()
+df_tmp = df_tmp.drop_duplicates("ImageId", keep='first')
+df_tmp.to_csv("./tmp.csv", index_label=False, index=False)
+
+df = pd.read_csv("./tmp.csv", index_col=0).dropna()
 
 # turn rle example into a list of ints
 rle = [int(i) for i in df['EncodedPixels']['55bd28f41.jpg'].split()]
 # turn list of ints into a list of (`start`, `length`) `pairs`
-pairs = list(zip(rle[0:-1:2], rle[1::2])) 
-pairs[:3]
+pairs = list(zip(rle[0:-1:2], rle[1::2]))
 
 start = pairs[0][0]
 print(f"Original start position: {start}")
@@ -24,76 +27,55 @@ print(f"Maps to this coordinate: {coordinate}")
 back = 768 * coordinate[1] + coordinate[0]
 print(f"And back: {back}")
 
-pixels = [(pixel_position % 768, pixel_position // 768) 
-                            for start, length in pairs 
+pixels = [(pixel_position % 768, pixel_position // 768)
+                            for start, length in pairs
                             for pixel_position in range(start, start + length)]
-pixels[:3]
+
 
 def rle_to_pixels(rle_code):
     '''
     Transforms a RLE code string into a list of pixels of a (768, 768) canvas
     '''
     rle_code = [int(i) for i in rle_code.split()]
-    pixels = [(pixel_position % 768, pixel_position // 768) 
-                 for start, length in list(zip(rle_code[0:-1:2], rle_code[1::2])) 
+    pixels = [(pixel_position % 768, pixel_position // 768)
+                 for start, length in list(zip(rle_code[0:-1:2], rle_code[1::2]))
                  for pixel_position in range(start, start + length)]
     return pixels
 
-# First three pixels of this particular bounding box:
-rle_to_pixels(df['EncodedPixels']['000155de5.jpg'])[0:3]
-
-# Create a matrix of shape (768, 768) full of zeros
-canvas = np.zeros((768, 768))
-
-# numpy arrays can't be indexed by a list of pairs [(x1, y1), (x2, y2)]
-# but it can be indexed with a tuple with ([x1, x2,..., xn], [y1, y2... yn])
-# tuple(zip(*)) does exactly this map.... 
-# ref: https://stackoverflow.com/questions/28491230/indexing-a-numpy-array-with-a-list-of-tuples
-canvas[tuple(zip(*pixels))] = 1
-
-plt.imshow(canvas);
-
-canvas = np.zeros((768, 768))
-pixels = rle_to_pixels(np.random.choice(df['EncodedPixels']))
-canvas[tuple(zip(*pixels))] = 1
-plt.imshow(canvas);
-
-# An image may have more than one row in the df, 
+# An image may have more than one row in the df,
 # Meaning that the image has more than one ship present
 # Here we merge those n-ships into the a continuos rle-code for the image....
 df = df.groupby("ImageId")[['EncodedPixels']].agg(lambda rle_codes: ' '.join(rle_codes)).reset_index()
 
-# ../input/train_v2/ for Windows, ./input/train_v2/ for MacOS
-load_img = lambda filename: np.array(PIL.Image.open(f"./input/train_v2/{filename}"))
 
-def apply_mask(image, mask):
-    for x, y in mask:
-        image[x, y, [0, 1]] = 255
-    return image
-
-
-img = load_img(df.loc[0, 'ImageId'])
-mask_pixels = rle_to_pixels(df.loc[0, 'EncodedPixels'])
-print(mask_pixels[0], mask_pixels[len(mask_pixels)-1])
-with open("file.txt", "w") as f:
-    print(df.loc[0, 'ImageId'], *sum((mask_pixels[0],mask_pixels[-1]),()), "ship",sep=',',file=f)
+for i in range(2000):
+    row_index = np.random.randint(len(df))  # take a random row from the df
+    mask_pixels = rle_to_pixels(df.loc[row_index, 'EncodedPixels'])
+    tuple_y, tuple_x = zip(*mask_pixels)
+    table_x = list(tuple_x)
+    table_y = list(tuple_y)
+    x_min = min(table_x)
+    y_min = min(table_y)
+    x_max = max(table_x)
+    y_max = max(table_y)
+    with open("entry_data.txt", "a") as f:
+        print(df.loc[row_index, 'ImageId'], x_min, y_min, x_max, y_max, "ship", sep=',',
+              file=f)
     f.close()
 
-img = apply_mask(img, mask_pixels)
-plt.imshow(img);
+    # NOTE: uncomment following part for checking if the Bounding Boxes are correctly selected
 
-w = 20
-h = 20
+    # load_img = lambda filename: np.array(PIL.Image.open(f"./input/train_v2/{filename}"))
+    # im = np.array(load_img(df.loc[row_index, 'ImageId']), dtype=np.uint8)
+    # # Create figure and axes
+    # fig, ax = plt.subplots(1)
+    # # Display the image
+    # ax.imshow(im)
+    # # Create a Rectangle patch
+    # rect = patches.Rectangle((x_min, y_min), x_max-x_min, y_max-y_min, linewidth=1, edgecolor='r', facecolor='none')
+    # # Add the patch to the Axes
+    # ax.add_patch(rect)
+    # ax.set_title(df.loc[row_index, 'ImageId'])
+    # plt.show()
 
-_, axes_list = plt.subplots(h, w, figsize=(2*w, 2*h))
-
-for axes in axes_list:
-    for ax in axes:
-        ax.axis('off')
-        row_index = np.random.randint(len(df)) # take a random row from the df
-        ax.imshow(apply_mask(load_img(df.loc[row_index, 'ImageId']), rle_to_pixels(df.loc[row_index, 'EncodedPixels'])))
-        ax.set_title(df.loc[row_index, 'ImageId'])
-        with open("file.txt", "a") as f:
-            print(df.loc[row_index, 'ImageId'],*sum((rle_to_pixels(df.loc[row_index, 'EncodedPixels'])[0],
-                        rle_to_pixels(df.loc[row_index, 'EncodedPixels'])[-1]), ()), "ship", sep=',', file=f)
-            f.close()
+print("Finished")
