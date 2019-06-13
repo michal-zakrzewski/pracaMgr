765d09fafaa5e6216b65db7bf13589589eaf1088
Michal Zakrzewski <michal.zakrzewski@westwing.pl>
2019-06-13 12:13:09 +0200

    Adding training model

--- keras_frcnn/roi_helpers.py
+++ keras_frcnn/roi_helpers.py
@@ -93,10 +93,9 @@ def calc_iou(R, img_data, C, class_mapping):
     X = np.array(x_roi)
     Y1 = np.array(y_class_num)
     print("\nY1 table: \n", Y1)
-    if len(Y1) == 1 or len(Y1) == 0 or len(Y1) == 2:
+    if len(Y1) == 1:
         print("\nGotcha! ", len(Y1))
-        Y1 = []
-        print("\nY1 table: \n", Y1)
+        Y1 = [[]]
     Y2 = np.concatenate([np.array(y_class_regr_label),np.array(y_class_regr_coords)],axis=1)
 
     return np.expand_dims(X, axis=0), np.expand_dims(Y1, axis=0), np.expand_dims(Y2, axis=0), IoUs
