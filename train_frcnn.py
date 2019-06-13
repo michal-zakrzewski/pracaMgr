01b197c31eb7ff15e8ffeb1c3e1e85c3cf5a1b91
Michal Zakrzewski <michal.zakrzewski@westwing.pl>
2019-06-13 11:17:59 +0200

    Adding training model

--- train_frcnn.py
+++ train_frcnn.py
@@ -255,7 +255,7 @@ for epoch_num in range(num_epochs):
             try:
                 selected_neg_samples = np.random.choice(neg_samples, C.num_rois - len(selected_pos_samples), replace=False).tolist()
             except ValueError:
-                print("Incorrect data! ", selected_neg_samples)
+                print("Incorrect data - debug point 1")
                 break
             except:
                 selected_neg_samples = np.random.choice(neg_samples, C.num_rois - len(selected_pos_samples), replace=True).tolist()
