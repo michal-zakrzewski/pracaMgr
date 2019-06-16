d0bca17bd90b26851d70f48e0ddd14c29c9cc6d6
Michal <zakrzewki@gmail.com>
2019-06-16 22:01:15 +0200

    Debug

--- train_frcnn.py
+++ train_frcnn.py
@@ -91,7 +91,7 @@ random.shuffle(all_imgs)
 
 num_imgs = len(all_imgs)
 
-train_imgs = [s for s in all_imgs]
+train_imgs = [s for s in all_imgs if s['imageset'] == 'trainval']
 val_imgs = [s for s in all_imgs if s['imageset'] == 'val']
 test_imgs = [s for s in all_imgs if s['imageset'] == 'test']
 
