d65610c915ec3b2295b0374b4637f45563f29818
Michal Zakrzewski <michal.zakrzewski@westwing.pl>
2019-07-01 08:33:28 +0200

    Background images decoder

--- GoogleDriveChecker.py
+++ GoogleDriveChecker.py
@@ -1,22 +1,26 @@
 from sys import platform
 import shutil
-import os
+import os.path
+from os import path
 
 if platform == "linux" or platform == "linux2":
     from IPython.core.display import display
-    path = str("/content/pracaMgr/input")
+    path = str("/content/pracaMgr")
 # TODO: Add possibility to upload weights from local to Google Drive
 elif platform == "darwin":
-    path = str("./input")
+    path = str("./")
+    from google.colab import drive
+    drive.mount('/content/drive', force_remount=True)
 elif platform == "win32":
-    path = str("../input")
+    path = str("../")
 
 # Create example file
-with open("Example_file.txt", "a") as f:
+with open("Example_file.txt", "w") as f:
     print("This is a test file", file=f)
 
 try:
     shutil.move(path + "/Example_file.txt", "/content/drive/My Drive/pracaMgr/Weights/Example_file.txt")
+    path.exist(path + "Example_file.txt")
 except Exception as e:
     print('Saving was not possible, sorry')
     print(e)
