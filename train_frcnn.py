e95d4569eb5891c7a224cac5ef213411f6a3e61f
Michal <zakrzewki@gmail.com>
2019-07-02 19:57:35 +0200

    Reformatting

--- train_frcnn.py
+++ train_frcnn.py
@@ -33,6 +33,8 @@ elif platform == "win32":
     path = str("../input")
 
 # tensorboard
+
+
 def write_log(callback, names, logs, batch_no):
     for name, value in zip(names, logs):
         summary = tf.Summary()
@@ -47,18 +49,23 @@ sys.setrecursionlimit(40000)
 
 parser = OptionParser()
 
-parser.add_option("-p", "--path", dest="train_path", help="Path to training data.")
-parser.add_option("--num_epochs", dest="num_epochs", help="Number of epochs.", default=40)
+parser.add_option("-p", "--path", dest="train_path",
+                  help="Path to training data.")
+parser.add_option("--num_epochs", dest="num_epochs",
+                  help="Number of epochs.", default=40)
 parser.add_option("--config_filename", dest="config_filename",
                   help="Location to store all the metadata related to the training (to be used when testing).",
                   default="config.pickle")
-parser.add_option("--output_weight_path", dest="output_weight_path", help="Output path for weights.", default='./input/weights.hdf5')
-parser.add_option("--input_weight_path", dest="input_weight_path", help="Input path for weights.")
+parser.add_option("--output_weight_path", dest="output_weight_path",
+                  help="Output path for weights.", default='./input/weights.hdf5')
+parser.add_option("--input_weight_path",
+                  dest="input_weight_path", help="Input path for weights.")
 
 (options, args) = parser.parse_args()
 
 if not options.train_path:   # if filename is not given
-    parser.error('Error: path to training data must be specified. Pass --path to command line')
+    parser.error(
+        'Error: path to training data must be specified. Pass --path to command line')
 
 # pass the settings from the command line, and persist them in the config object
 C = config.Config()
@@ -94,7 +101,8 @@ config_output_filename = options.config_filename
 
 with open(config_output_filename, 'wb') as config_f:
     pickle.dump(C, config_f)
-    print('Config has been written to {}, and can be loaded when testing to ensure correct results'.format(config_output_filename))
+    print('Config has been written to {}, and can be loaded when testing to ensure correct results'.format(
+        config_output_filename))
 
 random.shuffle(all_imgs)
 
@@ -109,9 +117,12 @@ print('Num val samples {}'.format(len(val_imgs)))
 print('Num test samples {}'.format(len(test_imgs)))
 
 # groundtruth anchor
-data_gen_train = data_generators.get_anchor_gt(train_imgs, classes_count, C, nn.get_img_output_length, K.image_dim_ordering(), mode='train')
-data_gen_val = data_generators.get_anchor_gt(val_imgs, classes_count, C, nn.get_img_output_length, K.image_dim_ordering(), mode='val')
-data_gen_test = data_generators.get_anchor_gt(test_imgs, classes_count, C, nn.get_img_output_length, K.image_dim_ordering(), mode='val')
+data_gen_train = data_generators.get_anchor_gt(
+    train_imgs, classes_count, C, nn.get_img_output_length, K.image_dim_ordering(), mode='train')
+data_gen_val = data_generators.get_anchor_gt(
+    val_imgs, classes_count, C, nn.get_img_output_length, K.image_dim_ordering(), mode='val')
+data_gen_test = data_generators.get_anchor_gt(
+    test_imgs, classes_count, C, nn.get_img_output_length, K.image_dim_ordering(), mode='val')
 
 if K.image_dim_ordering() == 'th':
     input_shape_img = (3, None, None)
@@ -131,7 +142,8 @@ num_anchors = len(C.anchor_box_scales) * len(C.anchor_box_ratios)
 rpn = nn.rpn(shared_layers, num_anchors)
 
 # detection network
-classifier = nn.classifier(shared_layers, roi_input, C.num_rois, nb_classes=len(classes_count), trainable=True)
+classifier = nn.classifier(shared_layers, roi_input, C.num_rois, nb_classes=len(
+    classes_count), trainable=True)
 
 model_rpn = Model(img_input, rpn[:2])
 model_classifier = Model([img_input, roi_input], classifier)
@@ -152,8 +164,10 @@ except:
 
 optimizer = Adam(lr=1e-5)
 optimizer_classifier = Adam(lr=1e-5)
-model_rpn.compile(optimizer=optimizer, loss=[losses.rpn_loss_cls(num_anchors), losses.rpn_loss_regr(num_anchors)])
-model_classifier.compile(optimizer=optimizer_classifier, loss=[losses.class_loss_cls, losses.class_loss_regr(len(classes_count)-1)], metrics={'dense_class_{}'.format(len(classes_count)): 'accuracy'})
+model_rpn.compile(optimizer=optimizer, loss=[losses.rpn_loss_cls(
+    num_anchors), losses.rpn_loss_regr(num_anchors)])
+model_classifier.compile(optimizer=optimizer_classifier, loss=[losses.class_loss_cls, losses.class_loss_regr(
+    len(classes_count)-1)], metrics={'dense_class_{}'.format(len(classes_count)): 'accuracy'})
 model_all.compile(optimizer='sgd', loss='mae')
 
 # Tensorboard log
@@ -191,21 +205,26 @@ for epoch_num in range(num_epochs):
         # try:
         # mean overlapping bboxes
         if len(rpn_accuracy_rpn_monitor) == epoch_length and C.verbose:
-            mean_overlapping_bboxes = float(sum(rpn_accuracy_rpn_monitor))/len(rpn_accuracy_rpn_monitor)
+            mean_overlapping_bboxes = float(
+                sum(rpn_accuracy_rpn_monitor))/len(rpn_accuracy_rpn_monitor)
             rpn_accuracy_rpn_monitor = []
-            print('\nAverage number of overlapping bounding boxes from RPN = {} for {} previous iterations'.format(mean_overlapping_bboxes, epoch_length))
+            print('\nAverage number of overlapping bounding boxes from RPN = {} for {} previous iterations'.format(
+                mean_overlapping_bboxes, epoch_length))
             if mean_overlapping_bboxes == 0:
-                print('RPN is not producing bounding boxes that overlap the ground truth boxes. Check RPN settings or keep training.')
+                print(
+                    'RPN is not producing bounding boxes that overlap the ground truth boxes. Check RPN settings or keep training.')
 
         # data generator X, Y, image
         X, Y, img_data = next(data_gen_train)
 
         loss_rpn = model_rpn.train_on_batch(X, Y)
-        write_log(callback, ['rpn_cls_loss', 'rpn_reg_loss'], loss_rpn, train_step)
+        write_log(callback, ['rpn_cls_loss',
+                             'rpn_reg_loss'], loss_rpn, train_step)
 
         P_rpn = model_rpn.predict_on_batch(X)
 
-        R = roi_helpers.rpn_to_roi(P_rpn[0], P_rpn[1], C, K.image_dim_ordering(), use_regr=True, overlap_thresh=0.7, max_boxes=300)
+        R = roi_helpers.rpn_to_roi(P_rpn[0], P_rpn[1], C, K.image_dim_ordering(
+        ), use_regr=True, overlap_thresh=0.7, max_boxes=300)
         # note: calc_iou converts from (x1,y1,x2,y2) to (x,y,w,h) format
         X2, Y1, Y2, IouS = roi_helpers.calc_iou(R, img_data, C, class_mapping)
 
@@ -235,12 +254,15 @@ for epoch_num in range(num_epochs):
             if len(pos_samples) < C.num_rois//2:
                 selected_pos_samples = pos_samples.tolist()
             else:
-                selected_pos_samples = np.random.choice(pos_samples, C.num_rois//2, replace=False).tolist()
+                selected_pos_samples = np.random.choice(
+                    pos_samples, C.num_rois//2, replace=False).tolist()
             try:
-                selected_neg_samples = np.random.choice(neg_samples, C.num_rois - len(selected_pos_samples), replace = False).tolist()
+                selected_neg_samples = np.random.choice(
+                    neg_samples, C.num_rois - len(selected_pos_samples), replace=False).tolist()
             except ValueError:
                 try:
-                    selected_neg_samples = np.random.choice(neg_samples, C.num_rois - len(selected_pos_samples), replace = True).tolist()
+                    selected_neg_samples = np.random.choice(
+                        neg_samples, C.num_rois - len(selected_pos_samples), replace=True).tolist()
                 except:
                     # The neg_samples is [[1 0 ]] only, therefore there's no negative sample
                     continue
@@ -255,8 +277,10 @@ for epoch_num in range(num_epochs):
             else:
                 sel_samples = random.choice(pos_samples)
 
-        loss_class = model_classifier.train_on_batch([X, X2[:, sel_samples, :]], [Y1[:, sel_samples, :], Y2[:, sel_samples, :]])
-        write_log(callback, ['detection_cls_loss', 'detection_reg_loss', 'detection_acc'], loss_class, train_step)
+        loss_class = model_classifier.train_on_batch(
+            [X, X2[:, sel_samples, :]], [Y1[:, sel_samples, :], Y2[:, sel_samples, :]])
+        write_log(callback, ['detection_cls_loss', 'detection_reg_loss',
+                             'detection_acc'], loss_class, train_step)
         train_step += 1
 
         losses[iter_num, 0] = loss_rpn[1]
@@ -278,12 +302,15 @@ for epoch_num in range(num_epochs):
             loss_class_regr = np.mean(losses[:, 3])
             class_acc = np.mean(losses[:, 4])
 
-            mean_overlapping_bboxes = float(sum(rpn_accuracy_for_epoch)) / len(rpn_accuracy_for_epoch)
+            mean_overlapping_bboxes = float(
+                sum(rpn_accuracy_for_epoch)) / len(rpn_accuracy_for_epoch)
             rpn_accuracy_for_epoch = []
 
             if C.verbose:
-                print('Mean number of bounding boxes from RPN overlapping ground truth boxes: {}'.format(mean_overlapping_bboxes))
-                print('Classifier accuracy for bounding boxes from RPN: {}'.format(class_acc))
+                print('Mean number of bounding boxes from RPN overlapping ground truth boxes: {}'.format(
+                    mean_overlapping_bboxes))
+                print(
+                    'Classifier accuracy for bounding boxes from RPN: {}'.format(class_acc))
                 print('Loss RPN classifier: {}'.format(loss_rpn_cls))
                 print('Loss RPN regression: {}'.format(loss_rpn_regr))
                 print('Loss Detector classifier: {}'.format(loss_class_cls))
@@ -304,18 +331,21 @@ for epoch_num in range(num_epochs):
 
             if curr_loss < best_loss:
                 if C.verbose:
-                    print('Total loss decreased from {} to {}, saving weights'.format(best_loss,curr_loss))
+                    print('Total loss decreased from {} to {}, saving weights'.format(
+                        best_loss, curr_loss))
                 model_all.save_weights(C.model_path)
                 try:
                     filename = "weights" + str(curr_loss)[:6] + ".hdf5"
                     shutil.copy(path + "/weights.hdf5", path + "/" + filename)
-                    os.rename(path + "/" + filename, "/content/drive/My Drive/pracaMgr/Weights/" + filename)
+                    os.rename(
+                        path + "/" + filename, "/content/drive/My Drive/pracaMgr/Weights/" + filename)
                 except Exception as e:
                     print("Saving was not possible, sorry")
                     print(e)
                 try:
-                    os.remove("/content/drive/My Drive/pracaMgr/Weights/weights" + str(best_loss)[:6] + ".hdf5")
-				except OSError as e:
+                    os.remove(
+                        "/content/drive/My Drive/pracaMgr/Weights/weights" + str(best_loss)[:6] + ".hdf5")
+                except OSError as e:
                     print("File removing was not possible")
                     print(e)
                 best_loss = curr_loss
