d438aed6a6395a87d01a2f10e5cc5c5db111e212
zakrzewki <45537492+zakrzewki@users.noreply.github.com>
2019-09-09 08:27:10 +0200

    Checking params

--- train_frcnn.py
+++ train_frcnn.py
@@ -244,7 +244,7 @@ for epoch_num in range(num_epochs):
         P_rpn = model_rpn.predict_on_batch(X)
 
         R = roi_helpers.rpn_to_roi(P_rpn[0], P_rpn[1], C, K.image_dim_ordering(
-        ), use_regr=True, overlap_thresh=0.7, max_boxes=300)
+        ), use_regr=True, overlap_thresh=0.6, max_boxes=400)
         # note: calc_iou converts from (x1,y1,x2,y2) to (x,y,w,h) format
         X2, Y1, Y2, IouS = roi_helpers.calc_iou(R, img_data, C, class_mapping)
 
