2d9586e8aff0799e4031139d4312c344ff9489e8
Michal Zakrzewski <michal.zakrzewski@westwing.pl>
2019-09-14 18:50:50 +0200

    Checking params

--- test_frcnn.py
+++ test_frcnn.py
@@ -172,7 +172,7 @@ all_imgs = []
 
 classes = {}
 
-bbox_threshold = 0.1
+bbox_threshold = 0.8
 
 visualise = True
 counter = 0
@@ -204,7 +204,7 @@ for idx, img_name in enumerate(sorted(os.listdir(img_path))):
     # get the feature maps and output from the RPN
     [Y1, Y2, F] = model_rpn.predict(X)
 
-    R = roi_helpers.rpn_to_roi(Y1, Y2, C, K.image_dim_ordering(), overlap_thresh=0.1)
+    R = roi_helpers.rpn_to_roi(Y1, Y2, C, K.image_dim_ordering(), overlap_thresh=0.5)
 
     # convert from (x1,y1,x2,y2) to (x,y,w,h)
     R[:, 2] -= R[:, 0]
