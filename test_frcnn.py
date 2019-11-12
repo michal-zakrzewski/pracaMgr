e02a5ecc22f5156e7611720d0d4ea339ac06ccc8
zakrzewki <45537492+zakrzewki@users.noreply.github.com>
2019-11-12 19:02:36 +0100

    Update test_frcnn.py

--- test_frcnn.py
+++ test_frcnn.py
@@ -119,9 +119,9 @@ def overlap_checker(x1, y1, x2, y2, all_coord):
         b = all_coord[start:start + 4]
         start += 4
         try:
-            if not (max(b[0], b[2]) <= min(x1, x2) or max(x1, x2) <= min(b[0], b[2]) or max(b[1], b[3]) <= min(y1, y2) or max(y1, y2) <= min(b[1], b[3])):
-                if not (min(x1, x2) < min(b[0], b[2]) and min(y1, y2) < min(b[1], b[3]) and max(x1, x2) > max(b[0], b[2]) and max(y1, y2) > max(b[1], b[3])):
-                    if not (min(b[0], b[2]) < min(x1, x2) and min(b[1], b[3]) < min(y1, y2) and max(b[0], b[2]) > max(x1, x2) and max(b[1], b[3]) > max(y1, y2)):
+            if (max(b[0], b[2]) <= min(x1, x2) or max(x1, x2) <= min(b[0], b[2]) or max(b[1], b[3]) <= min(y1, y2) or max(y1, y2) <= min(b[1], b[3])):
+                if not (min(x1, x2) <= min(b[0], b[2]) and min(y1, y2) <= min(b[1], b[3]) and max(x1, x2) => max(b[0], b[2]) and max(y1, y2) => max(b[1], b[3])):
+                    if not (min(b[0], b[2]) <= min(x1, x2) and min(b[1], b[3]) <= min(y1, y2) and max(b[0], b[2]) => max(x1, x2) and max(b[1], b[3]) => max(y1, y2)):
                         overlaps = False
                     else:
                         return True
@@ -312,7 +312,7 @@ for idx, img_name in enumerate(sorted(os.listdir(img_path))):
                 real_y1 = 0
             if real_y2 < 0:
                 real_y2 = 0
-            if all_coordinates is not None:
+            if all_coordinates:
                 if overlap_checker(real_x1, real_y1, real_x2, real_y2, all_coordinates):
                     saveValue = True
                     continue
